a24b9135644824801879dc305758577497711598
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-31 17:02:40 +0200

    feat: rotating animation

--- src/components/Box1/Box1.jsx
+++ src/components/Box1/Box1.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { motion } from "framer-motion"
 
 export default function Box1() {
+    const [isAnimating, setIsAnimating] = useState(false)
+
   return (
     <div className="box-container">
-        <motion.div className="box1"
-                    animate={{ 
-                        x: "900px"
-                     }}
-        >
-            <h1>Box 1</h1>
-        </motion.div>
+      <motion.div
+        className="box1"
+        animate={{
+          x: isAnimating ? 900 : 0, 
+          opacity: isAnimating ? 1 : 0.3,
+          rotate: isAnimating ? 360 : 0
+        }}
+        initial={{
+          opacity: 0.3,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 60,
+          //damping: 200// controls the speed of the transition
+        }}
+        onClick={() => setIsAnimating(!isAnimating)}
+      >
+        <h1>Box 1</h1>
+      </motion.div>
     </div>
-  )
+  );
 }
