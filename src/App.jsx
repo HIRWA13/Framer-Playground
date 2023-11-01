1133707232e4568951b64c4c814e91fc73fb661e
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-01 11:47:08 +0200

    feat: add box5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
 import Box3 from './components/Box3/Box3'
 import Box4 from './components/Box4/Box4'
+import Box5 from "./components/Box5/Box5";
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Box2 />
       <Box3 />
       <Box4 />
+      <Box5 />
     </>
   )
 }
